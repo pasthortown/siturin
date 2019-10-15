d9629cb4049c41f22997dafb836e6db862a299b8
pasthortown <pastorchessmaster@gmail.com>
2019-10-15 17:33:21 +0000

    ok

--- client/client-builder.h
+++ client/client-builder.h
@@ -1,4 +1,4 @@
 #!/bin/bash
-ng build --base-href "http://siturin-pruebas.turismo.gob.ec/" --prod
+ng build --base-href "http://desarrollo.siturin/" --prod
 rm -R ./../ClientBuild/
 cp -R ./dist/client/ ./../ClientBuild/
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
   production: false,
-  api_auth: 'http://ws-siturin-auth.turismo.gob.ec/',
-  api_dinardap: 'http://ws-siturin-dinardap.turismo.gob.ec/',
-  api_consultor: 'http://ws-siturin-consultor.turismo.gob.ec/',
-  api_exporter: 'http://ws-siturin-exporter.turismo.gob.ec/',
-  api_base: 'http://ws-siturin-base.turismo.gob.ec/',
-  api_alojamiento: 'http://ws-siturin-alojamiento.turismo.gob.ec/',
-  api_financiero: 'http://ws-siturin-financiero.turismo.gob.ec/',
-  api_mailer: 'http://ws-siturin-mailer.turismo.gob.ec/',
-  api_catastro: 'http://ws-siturin-catastro.turismo.gob.ec/',
+  api_auth: 'http://desarrollo.auth-siturin/',
+  api_dinardap: 'http://desarrollo.dinardap-siturin/',
+  api_consultor: 'http://desarrollo.consultor-siturin/',
+  api_exporter: 'http://desarrollo.exporter-siturin/',
+  api_base: 'http://desarrollo.base-siturin/',
+  api_alojamiento: 'http://desarrollo.alojamiento-siturin/',
+  api_financiero: 'http://desarrollo.financiero-siturin/',
+  api_mailer: 'http://desarrollo.mailer-siturin/',
+  api_catastro: 'http://desarrollo.catastro-siturin/',
+  api_alimentos: 'http://desarrollo.alimentos-siturin/',
   gmapapiKey: 'AIzaSyCGUwCcM-LKjRK4rjbBJ06_GLmX2LaYzfg',
 };
 
