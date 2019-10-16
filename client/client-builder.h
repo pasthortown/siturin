42071302aa846f45e26c022deef22f9868bddcd2
pasthortown <pastorchessmaster@gmail.com>
2019-10-16 23:03:41 +0000

    hola

--- client/client-builder.h
+++ client/client-builder.h
@@ -1,4 +1,4 @@
 #!/bin/bash
-ng build --base-href "http://desarrollo.siturin/" --prod
+ng build --base-href "http://siturin-pruebas.turismo.gob.ec/" --prod
 rm -R ./../ClientBuild/
 cp -R ./dist/client/ ./../ClientBuild/
