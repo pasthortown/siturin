097418a7c4a6bd7c828e0b847f0b5571bf80637f
pasthortown <pastorchessmaster@gmail.com>
2019-10-24 14:53:10 +0000

    hola

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  api_auth: 'http://ws-siturin-.auth.turismo.gob.ec/',
-  api_dinardap: 'http://ws-siturin-.dinardap.turismo.gob.ec/',
-  api_consultor: 'http://ws-siturin-.consultor.turismo.gob.ec/',
-  api_exporter: 'http://ws-siturin-.exporter.turismo.gob.ec/',
-  api_base: 'http://ws-siturin-.base.turismo.gob.ec/',
-  api_alojamiento: 'http://ws-siturin-.alojamiento.turismo.gob.ec/',
-  api_financiero: 'http://ws-siturin-.financiero.turismo.gob.ec/',
-  api_mailer: 'http://ws-siturin-.mailer.turismo.gob.ec/',
-  api_catastro: 'http://ws-siturin-.catastro.turismo.gob.ec/',
-  api_alimentos: 'http://ws-siturin-.alimentos.turismo.gob.ec/',
+  api_auth: 'http://ws-siturin-auth.turismo.gob.ec/',
+  api_dinardap: 'http://ws-siturin-dinardap.turismo.gob.ec/',
+  api_consultor: 'http://ws-siturin-consultor.turismo.gob.ec/',
+  api_exporter: 'http://ws-siturin-exporter.turismo.gob.ec/',
+  api_base: 'http://ws-siturin-base.turismo.gob.ec/',
+  api_alojamiento: 'http://ws-siturin-alojamiento.turismo.gob.ec/',
+  api_financiero: 'http://ws-siturin-financiero.turismo.gob.ec/',
+  api_mailer: 'http://ws-siturin-mailer.turismo.gob.ec/',
+  api_catastro: 'http://ws-siturin-catastro.turismo.gob.ec/',
+  api_alimentos: 'http://ws-siturin-alimentos.turismo.gob.ec/',
   gmapapiKey: 'AIzaSyCGUwCcM-LKjRK4rjbBJ06_GLmX2LaYzfg',
 };
 
