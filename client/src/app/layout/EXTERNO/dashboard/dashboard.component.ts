41d64df2506fee041f0d10727aa30970af0dc02b
pasthortown <pastorchessmaster@gmail.com>
2020-01-09 11:27:32 -0500

    hola

--- client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
+++ client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
@@ -2435,8 +2435,6 @@ export class DashboardComponent implements OnInit {
          });
       });
    }
-   console.log(this.capacitiesToShow);
-   console.log(this.tariffsToShow);
   }
 
   addComplementaryFoodService() {
