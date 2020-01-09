dfa6f584c210b5939e58eb36156ac2d135a68769
pasthortown <pastorchessmaster@gmail.com>
2020-01-09 10:44:39 -0500

    hola

--- client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
+++ client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
@@ -4998,8 +4998,8 @@ guardarDeclaracion() {
                });
                tariff.capacity_type_id = c1.capacity_type_id;
                tariff.isNewTariff = c1.isNewCapacity;
-               tariff.year = tariffTypeChild.year;
-               console.log(tariffTypeChild);
+               //tariff.year = tariffTypeChild.year;
+               console.log(this.tarifas);
                let newChild = {nombreDivision: nombreDivision, tariff: tariff, isReference: es_referencia, plazasHabitacion: plazasHabitacion};
                childs.push(newChild);
             });
