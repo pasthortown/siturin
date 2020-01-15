c258db391f552b6c5ba40d217cc3e160e975c879
pasthortown <pastorchessmaster@gmail.com>
2020-01-15 15:41:34 -0500

    hola

--- client/src/app/inactivacion/inactivacion.component.ts
+++ client/src/app/inactivacion/inactivacion.component.ts
@@ -9,6 +9,12 @@ import { ToastrManager } from 'ng6-toastr-notifications';
 import { Ruc } from '../models/BASE/Ruc';
 import { Establishment } from 'src/app/models/BASE/Establishment';
 import { saveAs } from 'file-saver/FileSaver';
+import { StateService } from 'src/app/services/CRUD/ALOJAMIENTO/state.service';
+import { State } from 'src/app/models/ALOJAMIENTO/State';
+import { Register } from 'src/app/models/ALOJAMIENTO/Register';
+import { RegisterService as CatastroRegisterService } from 'src/app/services/CRUD/CATASTRO/register.service';
+import { RegisterType } from 'src/app/models/ALOJAMIENTO/RegisterType';
+import { RegisterTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/registertype.service';
 
 @Component({
     selector: 'inactivacion-login',
@@ -23,6 +29,7 @@ export class InactivacionComponent implements OnInit {
   isRepresentantLegal = false;
   isRucOwner = false;
   identificationValidated = false;
+  estados_tramites: State[];
   consumoCedula = false;
   fechaExpedicion = 'porValidar';
   fechaExpiracion = 'porValidar';
@@ -48,15 +55,103 @@ export class InactivacionComponent implements OnInit {
   cuentaInterno = false;
   mainPhoneValidated: Boolean = false;
   secondaryPhoneValidated: Boolean = false;
+  idTramiteEstadoFilter = 0;
+  tramite = '-'; 
+  estados = [];
+  config: any = {
+   paging: true,
+   filtering: {filterString: ''},
+   className: ['table-striped', 'table-hover', 'table-bordered']
+  };
+  rows = [];
+  columns = [];
+  data = [];
+  currentPageMinturRegisters = 1;
+  lastPageMinturRegisters = 1;
+  recordsByPageRegisterMintur = 5;
+  registers_mintur = [];
+  registerMinturSelected: any = null;
+  register_types: any[] = [];
+  states: State[] = [];
+  specific_states: State[];
+  estado_tramite_selected_code: String = '1';
   
   constructor(private consultorDataService: ConsultorService,
     private router: Router, 
     private modalService: NgbModal,
     private toastr: ToastrManager,
+    private stateDataService: StateService,
+    private catastroRegisterDataService: CatastroRegisterService,
+    private register_typeDataService: RegisterTypeService,
     private dinardapDataService: DinardapService) {}
   
   ngOnInit() {
     this.user = new User();
+    this.getTramiteStates();
+  }
+
+  getStates() {
+   this.states = [];
+   this.stateDataService.get().then( r => {
+      this.states = r as State[];
+      this.getRegisterTypes();
+      this.getSpecificStates();
+   }).catch( e => { console.log(e); });
+  }
+
+  getSpecificStates() {
+   this.specific_states = [];
+   this.states.forEach(element => {
+      if (element.father_code == this.estado_tramite_selected_code) {
+         this.specific_states.push(element);
+      }
+   });
+  }
+
+  getRegisterTypes() {
+   this.register_typeDataService.get().then( r => {
+      this.register_types = r as RegisterType[];
+      this.getRegistersMintur();
+   }).catch( e => { console.log(e); });
+  }
+
+  getRegistersMintur() {
+   this.registers_mintur = [];
+   this.registerMinturSelected = new Register();
+   this.catastroRegisterDataService.searchByRuc(this.user.ruc.toString()).then( r => {
+      this.registers_mintur = r;
+      this.buildDataTable();
+   }).catch( e => console.log(e) );
+  }
+
+  getTramiteStates() {
+   this.estados_tramites = [];
+   this.stateDataService.get().then( r => {
+      r.forEach(element => {
+         if (element.father_code == '-') {
+            this.estados_tramites.push(element);
+         }
+      });
+   }).catch( e => { console.log(e); });
+  }
+
+  filterByTramiteState(tramite?: String) {
+   let filtroTexto: String = '';
+   this.estados_tramites.forEach(estado => {
+      if (estado.id == this.idTramiteEstadoFilter) {
+       filtroTexto = estado.name;
+      }
+   });
+   if(typeof tramite !== 'undefined') {
+      if (tramite == '-') {
+       this.config.filtering = {filterString: filtroTexto};
+      } else {
+       this.config.filtering = {filterString: filtroTexto + ' - ' + tramite};
+      }
+   } else {
+    this.config.filtering = {filterString: filtroTexto};
+   }
+   this.onChangeTable(this.config);
   }
 
   openDialog(content) {
@@ -311,7 +406,10 @@ export class InactivacionComponent implements OnInit {
            this.rucData = datosGenerales;
            if (this.ruc.tax_payer_type_id != 1) {
             this.rucData += datosRL;
-         }
+           }
+           if (this.SRIOK) {
+            this.startToGetInformationRegisters();
+           }
         });
      }).catch( e => {
         console.log(e);
@@ -322,6 +420,186 @@ export class InactivacionComponent implements OnInit {
    }
   }
 
+  buildDataTable() {
+   this.columns = [
+      {title: '', name: 'selected'},
+      {title: 'Establecimiento', name: 'comercial_name'},
+      {title: 'Fecha de Registro', name: 'as_turistic_date'},
+      {title: 'Número de Registro', name: 'register_code'},
+      {title: 'Provincia', name: 'ubication_main'},
+      {title: 'Cantón', name: 'ubication_sencond'},
+      {title: 'Parroquia', name: 'ubication_third'},
+      {title: 'Dirección', name: 'address'},
+      {title: 'Actividad', name: 'activity'},
+      {title: 'Categoría', name: 'category'},
+      {title: 'Clasificación', name: 'classification'},
+      {title: 'Estado', name: 'establishment_state'},
+   ];
+   const data = [];
+   const dataSITURIN = [];
+   const dataSIETE = [];
+   const dataOTHERS = [];
+   this.registers_mintur.forEach(item => {
+       let existe = false;
+       this.estados.forEach(element => {
+          if (element == item.establishment_state) {
+             existe = true;
+          }
+       });
+       if (!existe) {
+          this.estados.push(item.establishment_state);
+       }
+       const newItem = {
+          selected: '',
+          activity: item.activity,
+          address: item.address,
+          as_turistic_date: item.as_turistic_date,
+          category: item.category,
+          classification: item.classification,
+          comercial_name: item.comercial_name,
+          created_at: item.created_at,
+          email: item.email,
+          establishment_property_type: item.establishment_property_type,
+          establishment_ruc_code: item.establishment_ruc_code,
+          establishment_state: item.establishment_state,
+          georeference_latitude: item.georeference_latitude,
+          georeference_longitude: item.georeference_longitude,
+          id: item.id,
+          legal_representant_identification: item.legal_representant_identification,
+          legal_representant_name: item.legal_representant_name,
+          main_phone_number: item.main_phone_number,
+          max_areas: item.max_areas,
+          max_beds: item.max_beds,
+          max_capacity: item.max_capacity,
+          organization_type: item.organization_type,
+          register_code: item.register_code,
+          ruc: item.ruc,
+          ruc_state: item.ruc_state,
+          secondary_phone_number: item.secondary_phone_number,
+          system_source: item.system_source,
+          total_female: item.total_female,
+          total_male: item.total_male,
+          ubication_main: item.ubication_main,
+          ubication_sencond: item.ubication_sencond,
+          ubication_third: item.ubication_third,
+          updated_at: item.updated_at,
+          web: item.web,
+       };
+       if (newItem.system_source == 'SIETE') {
+          dataSIETE.push(newItem);
+       }
+       if (newItem.system_source == 'SITURIN') {
+          dataSITURIN.push(newItem);
+       }
+       if (newItem.system_source !== 'SITURIN' || newItem.system_source !== 'SIETE') {
+          dataOTHERS.push(newItem);
+       }
+   });
+   dataSITURIN.forEach(element => {
+      data.push(element);
+   });
+   dataSIETE.forEach(itemSIETE => {
+      let existeSITURIN = false;
+      dataSITURIN.forEach(itemSITURIN => {
+         if (itemSITURIN.establishment_ruc_code == itemSIETE.establishment_ruc_code) {
+          existeSITURIN = true;
+         }
+      });
+      if (!existeSITURIN) {
+         data.push(itemSIETE);
+      }
+   });
+   this.data = data;
+   this.onChangeTable(this.config);
+  }
+
+  onChangeTable(config: any, page: any = {page: this.currentPageMinturRegisters, itemsPerPage: this.recordsByPageRegisterMintur}): any {
+   if (config.filtering) {
+     Object.assign(this.config.filtering, config.filtering);
+   }
+   if (config.sorting) {
+     Object.assign(this.config.sorting, config.sorting);
+   }
+   const filteredData = this.changeFilter(this.data, this.config);
+   const sortedData = this.changeSort(filteredData, this.config);
+   this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
+  }
+
+  changeFilter(data: any, config: any): any {
+   this.rows.forEach(row => {
+      row.selected = '';
+   });
+   let filteredData: Array<any> = data;
+   this.columns.forEach((column: any) => {
+     if (column.filtering) {
+       filteredData = filteredData.filter((item: any) => {
+         return item[column.name].match(column.filtering.filterString);
+       });
+     }
+   });
+   if (!config.filtering) {
+     return filteredData;
+   }
+   if (config.filtering.columnName) {
+     return filteredData.filter((item:any) =>
+       item[config.filtering.columnName].match(this.config.filtering.filterString));
+   }
+   const tempArray: Array<any> = [];
+   filteredData.forEach((item: any) => {
+     let flag = false;
+     this.columns.forEach((column: any) => {
+       if (item[column.name].toString().match(this.config.filtering.filterString)) {
+         flag = true;
+       }
+     });
+     if (flag) {
+       tempArray.push(item);
+     }
+   });
+   filteredData = tempArray;
+   return filteredData;
+  }
+
+  changeSort(data: any, config: any): any {
+   if (!config.sorting) {
+     return data;
+   }
+   const columns = this.config.sorting.columns || [];
+   let columnName: string = void 0;
+   let sort: string = void 0;
+   for (let i = 0; i < columns.length; i++) {
+     if (columns[i].sort !== '' && columns[i].sort !== false) {
+       columnName = columns[i].name;
+       sort = columns[i].sort;
+     }
+   }
+   if (!columnName) {
+     return data;
+   }
+   return data.sort((previous:any, current:any) => {
+     if (previous[columnName] > current[columnName]) {
+       return sort === 'desc' ? -1 : 1;
+     } else if (previous[columnName] < current[columnName]) {
+       return sort === 'asc' ? -1 : 1;
+     }
+     return 0;
+   });
+  }
+
+  changePage(page: any, data: Array<any> = this.data):Array<any> {
+   const start = (page.page - 1) * page.itemsPerPage;
+   const end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
+   return data.slice(start, end);
+  }
+
+  onCellClick(event) {
+     //AQUI
+  }
+
+  startToGetInformationRegisters() {
+   this.getStates();
+  }
+   
   validateNombramiento(): Boolean {
     if(this.ruc.tax_payer_type_id <= 1) {
        return true;
--- client/src/app/inactivacion/inactivacion.module.ts
+++ client/src/app/inactivacion/inactivacion.module.ts
@@ -3,15 +3,30 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { StateService } from 'src/app/services/CRUD/ALOJAMIENTO/state.service';
 
 import { InactivacionRoutingModule } from './inactivacion-routing.module';
 import { InactivacionComponent } from './inactivacion.component';
 import { ConsultorService } from './../services/negocio/consultor.service';
 import { DinardapService } from '../services/negocio/dinardap.service';
+import { RegisterService as CatastroRegisterService } from 'src/app/services/CRUD/CATASTRO/register.service';
+import { RegisterTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/registertype.service';
 
 @NgModule({
-    imports: [CommonModule, InactivacionRoutingModule, FormsModule, HttpModule],
+    imports: [CommonModule, 
+        InactivacionRoutingModule, 
+        FormsModule, 
+        NgbModule,
+        Ng2TableModule,
+        HttpModule],
     declarations: [InactivacionComponent],
-    providers: [ConsultorService, NgbModal, DinardapService]
+    providers: [ConsultorService, 
+        NgbModal, 
+        StateService, 
+        CatastroRegisterService,
+        RegisterTypeService,
+        DinardapService]
 })
 export class InactivacionModule {}
