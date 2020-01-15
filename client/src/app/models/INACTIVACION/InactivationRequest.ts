a55dec73178603249ee1f57c5c04da65af38c1d5
pasthortown <pastorchessmaster@gmail.com>
2020-01-15 16:51:54 -0500

    hola

--- client/src/app/models/INACTIVACION/InactivationRequest.ts
+++ client/src/app/models/INACTIVACION/InactivationRequest.ts
@@ -1,10 +1,11 @@
 export class InactivationRequest {
    id: number;
    ruc: String;
-   register_ids: String;
-   comments: String;
+   ubication_id: number;
    contact_phone_main_number: String;
    contact_phone_seconday_number: String;
    email: String;
    identification: String;
+   address: String;
+   comments: String;
 }
