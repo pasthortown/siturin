231452711e1b082dfd97e314a401b6586ececdea
pasthortown <pastorchessmaster@gmail.com>
2020-01-15 11:03:13 -0500

    hola

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -19,6 +19,10 @@ export class RegisterComponent implements OnInit {
   ruc: Ruc = new Ruc();
   busy: Promise<any>;
   esperando: Boolean;
+  isRepresentantLegal = true;
+  isRucOwner = true;
+//   isRepresentantLegal = false;
+//   isRucOwner = false;
   rucInactive = true;
   identificationValidated = false; 
   consumoCedula = false;
@@ -36,6 +40,7 @@ export class RegisterComponent implements OnInit {
   fechaExpiracion = 'porValidar';
   fechaNacimiento = 'porValidar';
   fechaIngresada = '';
+  razon_social = '';
 
   aleatorio = 0;
 
@@ -201,44 +206,122 @@ export class RegisterComponent implements OnInit {
    }
 
    checkRuc() {
-    this.ruc.number = this.ruc.number.replace(/[^\d]/, '');
-    if (this.ruc.number.length !== 13) {
-      this.rucValidated = false;
-      this.consumoRuc = false;
-      return;
+      this.ruc.number = this.ruc.number.replace(/[^\d]/, '');
+      if (this.ruc.number.length !== 13) {
+        this.rucValidated = false;
+        this.consumoRuc = false;
+        return;
+      }
+      if (this.consumoRuc && this.SRIOK) {
+         return;
+      }
+      this.rucData = '<div class=\"progress mb-3\"><div class=\"progress-bar progress-bar-striped progress-bar-animated bg-warning col-12\">Espere...</div></div><div class="col-12 text-center"><strong>Conectándose al SRI...</strong></div>';
+      if (!this.consumoRuc && this.identificationValidated) {
+        this.rucValidated = true;
+        this.consumoRuc = true;
+        this.rucInactive = true;
+        this.dinardapDataService.get_RUC(this.ruc.number).then( r => {
+          this.SRIOK = true; 
+          this.rucValidated = true;
+          const itemsDetalles_SRI_RUC = r.sri_ruc.original.entidades.entidad.filas.fila.columnas.columna;
+          const itemsDetalles_SRI_RUC_COMPLETO = r.sri_ruc_completo.original.entidades.entidad;
+          this.rucData = '';
+          let datosGenerales = '';
+          let datosRL = '';
+          itemsDetalles_SRI_RUC_COMPLETO.forEach(entidad => {
+             if (entidad.nombre == 'Actividad Economica') {
+                const AE = entidad.filas.fila.columnas.columna;
+                AE.forEach(element => {
+                   if (element.campo == 'actividadGeneral') {
+                   }
+                });
+             }
+             if (entidad.nombre == 'Contribuyente Datos Completo') {
+                const DC = entidad.filas.fila.columnas.columna;
+                DC.forEach(element => {
+                   if (element.campo == 'razonSocial') {
+                      this.razon_social = element.valor;
+                      datosGenerales += '<strong>Razón Social: </strong> ' + element.valor + '<br/>';
+                   }
+                   if (element.campo == 'email') {
+                      if (JSON.stringify(element.valor) !== '{}') {
+                      }
+                   }
+                   if (element.campo == 'telefonoDomicilio') {
+                      if (JSON.stringify(element.valor) !== '{}') {
+                      }
+                   }
+                });
+             }
+             if (entidad.nombre == 'Representante Legal') {
+                const RL = entidad.filas.fila.columnas.columna;
+                RL.forEach(element => {
+                   if (element.campo == 'identificacion') {
+                      if (JSON.stringify(element.valor) !== '{}') {
+                         this.ruc.person_representative.identification = element.valor;
+                         this.checkIdentificationRepresentant();
+                      }
+                   }
+                   if (element.campo == 'nombre') {
+                     datosRL += '<strong>Nombre Representante Legal: </strong> ' + element.valor + '<br/>';
+                   }
+                });
+             }
+          });
+          itemsDetalles_SRI_RUC.forEach(element => {
+             if (element.campo == 'estadoContribuyente') {
+                if (element.valor === 'ACTIVO') {
+                   this.rucInactive = false;
+                } else {
+                   this.rucInactive = true;
+                }
+             }
+             if (element.campo == 'fechaInscripcionRuc') {
+             }
+             if (element.campo == 'fechaActualizacion') {
+             }
+             if (element.campo == 'obligado') {
+                if (element.valor == 'N') {
+                   this.ruc.baised_accounting = false;
+                } else {
+                   this.ruc.baised_accounting = true;
+                }
+             }
+             if (element.campo == 'personaSociedad') {
+                if (element.valor == 'PNL') {
+                   this.ruc.tax_payer_type_id = 1;
+                   this.checkRazonSocial();
+                } else {
+                   this.ruc.tax_payer_type_id = 2;
+                }
+             }
+             this.rucData = datosGenerales;
+             if (this.ruc.tax_payer_type_id != 1) {
+              this.rucData += datosRL;
+           }
+          });
+       }).catch( e => {
+          console.log(e);
+          this.rucData = '<div class="alert alert-danger" role="alert">El SRI, no respondió. Vuelva a intentarlo.</div>';
+          this.consumoRuc = false;
+          this.SRIOK = false;
+       });
+     }
     }
-    if (this.consumoRuc && this.SRIOK) {
-       return;
-    }
-    this.rucData = '<div class=\"progress mb-3\"><div class=\"progress-bar progress-bar-striped progress-bar-animated bg-warning col-12\">Espere...</div></div><div class="col-12 text-center"><strong>Conectándose al SRI...</strong></div>';
-    if (!this.consumoRuc && this.identificationValidated) {
-      this.rucValidated = true;
-      this.consumoRuc = true;
-      this.rucInactive = true;
-      this.dinardapDataService.get_RUC(this.ruc.number).then( r => {
-         const sri_ruc_registros = r.sri_ruc.original.entidades.entidad.filas.fila.columnas.columna;
-         sri_ruc_registros.forEach(element => {
-            if (element.campo === 'estadoContribuyente') {
-               if (element.valor === 'ACTIVO') {
-                  this.rucData = '<strong>Estado del RUC: </strong> CONFIRMADO';
-                  this.rucInactive = false;
-                  this.SRIOK = true;
-               } else {
-                  this.toastr.errorToastr('El RUC ingresado no es Activo.', 'SRI');
-                  this.rucData = 'RUC INACTIVO';
-                  this.rucInactive = false;
-                  this.SRIOK = true;
-               }
-            }
-         });
-      }).catch( e => {
-         this.toastr.errorToastr('El RUC ingresado no es correcto.', 'SRI');
-         this.rucData = '<div class="alert alert-danger" role="alert">El SRI, no respondió. Vuelva a intentarlo.</div>';
-         this.consumoRuc = false;
-         this.SRIOK = false;
-      });
-   }
-  }
+
+    checkRazonSocial() {
+      // this.isRucOwner = false;
+      // if (this.razon_social == this.user.name) {
+      //   this.isRucOwner = true;
+      // }
+     }
+  
+    checkIdentificationRepresentant() { 
+      // this.isRepresentantLegal = false; 
+      // if (this.ruc.person_representative.identification == this.user.identification) {
+      //   this.isRepresentantLegal = true;
+      // }
+     }
 
   checkEmail(): Boolean {
     const isOk = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.user.email.toString());
