f5975ed600686b4ba93e5a5433fd7e4cdeb760ed
pasthortown <pastorchessmaster@gmail.com>
2020-01-21 15:37:33 -0500

    hola

--- client/src/app/inactivacion/inactivacion.component.ts
+++ client/src/app/inactivacion/inactivacion.component.ts
@@ -444,8 +444,6 @@ export class InactivacionComponent implements OnInit {
    }
 
    guardarDeclaracion() {
-      console.log(this.selected_register_data);
-      return;
       if(!this.validateDeclaration) {
          this.toastr.errorToastr('La información ingresada es incorrecta.', 'Declaración');
          return;
@@ -476,44 +474,45 @@ export class InactivacionComponent implements OnInit {
       });
       this.guardando = true;
       if (this.establishment_selected.id == 0) {
-         this.establishmentDataService.register_min_establishment(this.establishment_selected).then(r => {
-            const newEstablishmentRegistered = r as Establishment;
-            //this.catastroRegisterDataService.update_ruc_code_id()//AQUI
+         this.establishmentDataService.register_min_establishment(this.establishment_selected).then(resp_est => {
+            const newEstablishmentRegistered = resp_est as Establishment;
             this.establishment_selected = newEstablishmentRegistered;
             this.declaration_selected.establishment_id = this.establishment_selected.id;
-            this.declarationDataService.register_data(this.declaration_selected).then( r => {
-               if ( r === '0' ) {
+            this.catastroRegisterDataService.update_ruc_code_id(this.selected_register_data.id, this.establishment_selected.ruc_code_id, this.establishment_selected.sri_state).then( resp_cat => {
+               this.declarationDataService.register_data(this.declaration_selected).then( r => {
+                  if ( r === '0' ) {
+                     this.toastr.errorToastr('Existe conflicto la información proporcionada.', 'Declaración');
+                     return;
+                  }
+                  const declarationSaved = r as Declaration;
+                  this.balance.declaration_id = declarationSaved.id;
+                  if (this.balance.id == 0) {
+                     this.declarationAttachmentDataService.post(this.balance).then( r1 => {
+                        this.toastr.successToastr('Datos guardados satisfactoriamente.', 'Declaración');
+                        this.refreshDeclaracion();
+                        this.mostrarDataDeclaration = false;
+                        this.guardando = false;
+                     }).catch( e => {
+                        console.log(e);
+                        this.guardando = false;
+                     });
+                  } else {
+                     this.declarationAttachmentDataService.put(this.balance).then( r1 => {
+                        this.toastr.successToastr('Datos guardados satisfactoriamente.', 'Declaración');
+                        this.refreshDeclaracion();
+                        this.mostrarDataDeclaration = false;
+                        this.guardando = false;
+                     }).catch( e => { 
+                        console.log(e);
+                        this.guardando = false;
+                     });
+                  }
+               }).catch( e => {
+                  this.guardando = false;
                   this.toastr.errorToastr('Existe conflicto la información proporcionada.', 'Declaración');
                   return;
-               }
-               const declarationSaved = r as Declaration;
-               this.balance.declaration_id = declarationSaved.id;
-               if (this.balance.id == 0) {
-                  this.declarationAttachmentDataService.post(this.balance).then( r1 => {
-                     this.toastr.successToastr('Datos guardados satisfactoriamente.', 'Declaración');
-                     this.refreshDeclaracion();
-                     this.mostrarDataDeclaration = false;
-                     this.guardando = false;
-                  }).catch( e => {
-                     console.log(e);
-                     this.guardando = false;
-                  });
-               } else {
-                  this.declarationAttachmentDataService.put(this.balance).then( r1 => {
-                     this.toastr.successToastr('Datos guardados satisfactoriamente.', 'Declaración');
-                     this.refreshDeclaracion();
-                     this.mostrarDataDeclaration = false;
-                     this.guardando = false;
-                  }).catch( e => { 
-                     console.log(e);
-                     this.guardando = false;
-                  });
-               }
-            }).catch( e => {
-               this.guardando = false;
-               this.toastr.errorToastr('Existe conflicto la información proporcionada.', 'Declaración');
-               return;
-            });
+               });
+            }).catch( e => { console.log(e); });
          }).catch( e => { console.log(e); });
       } else {
          this.declaration_selected.establishment_id = this.establishment_selected.id;
