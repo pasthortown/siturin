d95c00f20433f64070eb56d75e38f6a203312154
pasthortown <pastorchessmaster@gmail.com>
2020-01-22 08:45:13 -0500

    hola

--- client/src/app/models/INACTIVACION/InactivationRequestAttachment.ts
+++
@@ -1,7 +0,0 @@
-export class InactivationRequestAttachment {
-   id: number;
-   inactivation_request_attachment_file_type: String;
-   inactivation_request_attachment_file_name: String;
-   inactivation_request_attachment_file: String;
-   inactivation_request_id: number;
-}
--- client/src/app/models/INACTIVACION/InactivationRequestDeclaration.ts
+++
@@ -1,5 +0,0 @@
-export class InactivationRequestDeclaration {
-   id: number;
-   declaration_id: number;
-   inactivation_request_id: number;
-}
