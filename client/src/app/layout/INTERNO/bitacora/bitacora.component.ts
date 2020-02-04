206c807163f7236bb3183c1fd566da9a608deecf
pasthortown <pastorchessmaster@gmail.com>
2020-02-04 16:27:57 -0500

    hola

--- client/src/app/layout/INTERNO/bitacora/bitacora.component.ts
+++ client/src/app/layout/INTERNO/bitacora/bitacora.component.ts
@@ -244,8 +244,7 @@ export class BitacoraComponent implements OnInit {
     });
    });
    this.dataRegisters = data;
-   console.log(this.dataRegisters);
-   //this.onChangeTable(this.config);
+   this.onChangeTableRegisters(this.config);
   }
 
   buscarBitacora() {
@@ -426,6 +425,86 @@ export class BitacoraComponent implements OnInit {
    }
  
    changePage(page: any, data: Array<any> = this.data):Array<any> {
+    const start = (page.page - 1) * page.itemsPerPage;
+    const end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
+    return data.slice(start, end);
+   }
+
+   onChangeTableRegisters(config: any, event?): any {
+    const page: any = {page: this.currentPageRegisters, itemsPerPage: this.recordsByPageRegisters};
+    if (config.filtering) {
+      Object.assign(this.config.filtering, config.filtering);
+    }
+    if (config.sorting) {
+      Object.assign(this.config.sorting, config.sorting);
+    }
+    const filteredData = this.changeFilterRegisters(this.dataRegisters, this.config);
+    const sortedData = this.changeSortRegisters(filteredData, this.config);
+    this.rowsRegisters = page && config.paging ? this.changePageRegisters(page, sortedData) : sortedData;
+   }
+ 
+   changeFilterRegisters(data: any, config: any): any {
+    this.rowsRegisters.forEach(row => {
+       row.selected = '';
+    });
+    let filteredData: Array<any> = data;
+    this.columnsRegisters.forEach((column: any) => {
+      if (column.filtering) {
+        filteredData = filteredData.filter((item: any) => {
+          return item[column.name].toUpperCase().match(column.filtering.filterString.toUpperCase());
+        });
+      }
+    });
+    if (!config.filtering) {
+      return filteredData;
+    }
+    if (config.filtering.columnName) {
+      return filteredData.filter((item:any) =>
+        item[config.filtering.columnName].match(this.config.filtering.filterString));
+    }
+    const tempArray: Array<any> = [];
+    filteredData.forEach((item: any) => {
+      let flag = false;
+      this.columnsRegisters.forEach((column: any) => {
+        if (item[column.name].toString().match(this.config.filtering.filterString)) {
+          flag = true;
+        }
+      });
+      if (flag) {
+        tempArray.push(item);
+      }
+    });
+    filteredData = tempArray;
+    return filteredData;
+   }
+ 
+   changeSortRegisters(data: any, config: any): any {
+    if (!config.sorting) {
+      return data;
+    }
+    const columns = this.config.sorting.columns || [];
+    let columnName: string = void 0;
+    let sort: string = void 0;
+    for (let i = 0; i < columns.length; i++) {
+      if (columns[i].sort !== '' && columns[i].sort !== false) {
+        columnName = columns[i].name;
+        sort = columns[i].sort;
+      }
+    }
+    if (!columnName) {
+      return data;
+    }
+    return data.sort((previous:any, current:any) => {
+      if (previous[columnName] > current[columnName]) {
+        return sort === 'desc' ? -1 : 1;
+      } else if (previous[columnName] < current[columnName]) {
+        return sort === 'asc' ? -1 : 1;
+      }
+      return 0;
+    });
+   }
+ 
+   changePageRegisters(page: any, data: Array<any> = this.dataRegisters):Array<any> {
     const start = (page.page - 1) * page.itemsPerPage;
     const end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
     return data.slice(start, end);
