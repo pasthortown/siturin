90db3dc85c08fd3331b2292ec14d40f8e8a24f01
pasthortown <pastorchessmaster@gmail.com>
2020-02-04 11:11:12 -0500

    hola

--- client/src/app/layout/INTERNO/bitacora/bitacora.module.ts
+++ client/src/app/layout/INTERNO/bitacora/bitacora.module.ts
@@ -4,8 +4,18 @@ import { NgModule } from '@angular/core';
 import { BitacoraRoutingModule } from './bitacora-routing.module';
 import { BitacoraComponent } from './bitacora.component';
 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
-  imports: [CommonModule, BitacoraRoutingModule],
+  imports: [CommonModule,
+    BitacoraRoutingModule,
+    FormsModule, 
+    NgbModule,
+    Ng2TableModule,
+    HttpModule],
   declarations: [BitacoraComponent]
 })
 export class BitacoraModule {}
