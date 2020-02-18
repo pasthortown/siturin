343be875bb18c31f4e19727384a19d98cd97eecd
pasthortown <pastorchessmaster@gmail.com>
2020-02-18 17:06:52 -0500

    hola

--- client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
+++ client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
@@ -3352,7 +3352,7 @@ export class DashboardComponent implements OnInit {
 guardarDeclaracion() {
    let minim_year_declaration = 0;
    if (this.establishment_selected.as_turistic_register_date != null || typeof this.establishment_selected.as_turistic_register_date != 'undefined') {
-      let minim_year_declaration = this.establishment_selected.as_turistic_register_date.getFullYear();   
+      minim_year_declaration = this.establishment_selected.as_turistic_register_date.getFullYear();   
    }
    if (this.declaration_selected.year < minim_year_declaration) {
       this.toastr.errorToastr('Existe inconsistencia con el año de la declaración.', 'Declaración');
