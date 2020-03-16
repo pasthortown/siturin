fe0cde8f05a8fc7a9c7fb9052aa77358fe270c22
pasthortown <pastorchessmaster@gmail.com>
2020-03-16 09:16:52 -0500

    hola

--- client/src/app/services/negocio/normativa.service.ts
+++ client/src/app/services/negocio/normativa.service.ts
@@ -15,7 +15,7 @@ export class NormativaService {
    get_actvidades(): Promise<any> {
       return this.http.get(this.actividades_URL).toPromise()
       .then( r => {
-         return r.json();
+         return r;
       }).catch( error => { this.handledError(error.json());  });
    }
 
