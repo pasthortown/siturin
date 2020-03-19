b2ce8c8d5f0a1aef7cee228ebc11dbeb8a1f4e06
pasthortown <pastorchessmaster@gmail.com>
2020-03-19 13:03:29 -0500

    hola

--- client/src/app/layout/INTERNO/normativa/normativa.component.ts
+++ client/src/app/layout/INTERNO/normativa/normativa.component.ts
@@ -1,4 +1,5 @@
 import { RegisterRequisite } from './../../../models/ALOJAMIENTO/RegisterRequisite';
+import { RegisterRequisite as RegisterABRequisite } from './../../../models/ALIMENTOSBEBIDAS/RegisterRequisite';
 import { RequisiteService } from './../../../services/CRUD/ALOJAMIENTO/requisite.service';
 import { RequisiteService as RequisiteABService} from './../../../services/CRUD/ALIMENTOSBEBIDAS/requisite.service';
 import { RegisterTypeService } from './../../../services/CRUD/ALOJAMIENTO/registertype.service';
@@ -125,6 +126,10 @@ export class NormativaComponent implements OnInit {
     });
   }
 
+  calcTotalPoints() {
+
+  }
+  
   getRequisites() {
     this.requisites = [];
     if (this.actividadSelected == '1') {
@@ -159,7 +164,40 @@ export class NormativaComponent implements OnInit {
     }
     if (this.actividadSelected == '2') {
       this.requisiteABDataService.get_filtered(this.register_type_id).then( r=> {
+        this.totalAbPointsSelected = 0;
+        this.totalAviable = 0;
+        this.totalABPuntosShown = 0;
+        this.categoryAB = 0;
 
+        const requisitesByRegisterType = r as any[];
+        requisitesByRegisterType.forEach(element => {
+          const newRegisterRequisite = new RegisterABRequisite();
+          newRegisterRequisite.to_approve = element.to_approve;
+          newRegisterRequisite.score = element.score;
+          newRegisterRequisite.requisite_name = element.name;
+          newRegisterRequisite.requisite_id = element.id;
+          newRegisterRequisite.fullfill = true;
+          newRegisterRequisite.requisite_code = element.code;
+          newRegisterRequisite.mandatory = element.mandatory;
+          newRegisterRequisite.id = element.id;
+          newRegisterRequisite.requisite_father_code = element.father_code;
+          newRegisterRequisite.level = element.code.split('.').length;
+          newRegisterRequisite.HTMLtype = element.type;
+          newRegisterRequisite.fullfill = false;
+          if (newRegisterRequisite.HTMLtype == 'YES / NO') {
+              newRegisterRequisite.value = '0';
+          }
+          if (newRegisterRequisite.HTMLtype == 'NUMBER') {
+              newRegisterRequisite.value = '0';
+          }
+          if (newRegisterRequisite.HTMLtype == 'TRUE / FALSE') {
+              newRegisterRequisite.value = 'false';
+          }
+          this.requisites.push(newRegisterRequisite);
+        });
+        this.mostrarRequisitos = true;
+        this.calcTotalPoints();
+        this.sortArray(this.requisites);
       }).catch( e => { console.log(e); });
     }
   }
