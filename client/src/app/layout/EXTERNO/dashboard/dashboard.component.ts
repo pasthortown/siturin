42818a84722708f976dd28d0b3ab490b4b715271
pasthortown <pastorchessmaster@gmail.com>
2020-04-01 13:22:31 -0500

    hola

--- client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
+++ client/src/app/layout/EXTERNO/dashboard/dashboard.component.ts
@@ -452,7 +452,7 @@ export class DashboardComponent implements OnInit {
   newRepresentanteVentas: SalesRepresentative = new SalesRepresentative();
   newTuristicGuide: TourGuide = new TourGuide();
   newTuristicTransport: TuristicTransport = new TuristicTransport();
-  activateOperationIntermediation = true;
+  activateOperationIntermediation = false;
   activateAlojamiento = true;
   activateAlimentosBebidas = true;
   rucDataSalesRepresentative = 'CONECTÁNDOSE AL SRI...';
