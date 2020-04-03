44f6de952a4be25d6a95873519181c85cd48ffc8
pasthortown <pastorchessmaster@gmail.com>
2020-04-03 15:08:04 -0500

    hola

--- client/src/app/layout/EXTERNO/cliente-externo/cliente-externo.module.ts
+++ client/src/app/layout/EXTERNO/cliente-externo/cliente-externo.module.ts
@@ -1,3 +1,4 @@
+import { RucComponent } from './../../CRUD/BASE/Ruc/ruc.component';
 import { EstablishmentComponent } from './../../CRUD/BASE/Establishment/establishment.component';
 import { DataComponent } from './../../client-data-components/data/data.component';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,6 @@ import { ClienteExternoComponent } from './cliente-externo.component';
 
 @NgModule({
   imports: [CommonModule, ClienteExternoRoutingModule],
-  declarations: [ClienteExternoComponent, DataComponent, EstablishmentComponent]
+  declarations: [ClienteExternoComponent, DataComponent, EstablishmentComponent, RucComponent]
 })
 export class ClienteExternoModule {}
