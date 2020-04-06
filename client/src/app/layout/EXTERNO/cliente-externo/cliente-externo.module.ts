95b94da5b8d294a8e9c832b635d36de1aa52a2fb
pasthortown <pastorchessmaster@gmail.com>
2020-04-06 09:05:31 -0500

    hola

--- client/src/app/layout/EXTERNO/cliente-externo/cliente-externo.module.ts
+++ client/src/app/layout/EXTERNO/cliente-externo/cliente-externo.module.ts
@@ -1,3 +1,5 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RegistersDataComponent } from './../../client-data-components/registers-data/registers-data.component';
 import { OptionsDataComponent } from './../../client-data-components/options-data/options-data.component';
 import { AgreementDataComponent } from './../../client-data-components/agreement-data/agreement-data.component';
@@ -14,8 +16,6 @@ import { DeclarationDataComponent } from './../../client-data-components/declara
 import { TuristicDataComponent } from './../../client-data-components/turistic-data/turistic-data.component';
 import { RucDataComponent } from './../../client-data-components/ruc-data/ruc-data.component';
 import { EstablishmentDataComponent } from './../../client-data-components/establishment-data/establishment-data.component';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 
 import { ClienteExternoRoutingModule } from './cliente-externo-routing.module';
 import { ClienteExternoComponent } from './cliente-externo.component';
