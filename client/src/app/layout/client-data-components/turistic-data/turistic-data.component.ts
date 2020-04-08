9618e569173e50e98e0c0e2446d73626c3ee4f67
pasthortown <pastorchessmaster@gmail.com>
2020-04-07 23:18:14 -0500

    hola

--- client/src/app/layout/client-data-components/turistic-data/turistic-data.component.ts
+++ client/src/app/layout/client-data-components/turistic-data/turistic-data.component.ts
@@ -41,46 +41,45 @@ export class TuristicDataComponent implements OnInit {
 
   filter_registers_by_ruc() {
     const registros_establecimiento = [];
-    const register_types_aviable = [];
+    const register_codes_aviable = [];
     this.establishment_registers = [];
-    console.log(this.registers_by_ruc);
     this.registers_by_ruc.forEach(element => {
       if (element.establishment.id == this.establishment.id) {
         registros_establecimiento.push(element);
         let existe = false;
-        register_types_aviable.forEach(reg_type => {
-          if (reg_type == element.register.register_type_id) {
+        register_codes_aviable.forEach(reg_code => {
+          if (reg_code == element.register.code) {
             existe = false;
           }
         });
         if (!existe) {
-          register_types_aviable.push(element.register.register_type_id);
+          register_codes_aviable.push(element.register.code);
         }
       }  
     });
-    register_types_aviable.forEach(reg_type => {
-      let last_register_by_type = null;
+    register_codes_aviable.forEach(reg_code => {
+      let last_register_by_code = null;
       registros_establecimiento.forEach(element => {
-        if (element.register.register_type_id == reg_type) {
-          if (last_register_by_type == null) {
-            last_register_by_type = element;
+        if (element.register.code == reg_code) {
+          if (last_register_by_code == null) {
+            last_register_by_code = element;
           } else {
-            const fecha_e1 = new Date(last_register_by_type.register.created_at.toString());
+            const fecha_e1 = new Date(last_register_by_code.register.created_at.toString());
             const fecha_e2 = new Date(element.register.created_at.toString());
             if (fecha_e2.getTime() > fecha_e1.getTime()) {
-              last_register_by_type = element;
+              last_register_by_code = element;
             }
           }
         }
       });
       let existe = false;
       this.establishment_registers.forEach(element => {
-        if (element == last_register_by_type) {
+        if (element == last_register_by_code) {
           existe = true;
         }
       });
       if (!existe) {
-        this.establishment_registers.push(last_register_by_type);
+        this.establishment_registers.push(last_register_by_code);
       }
     });
   }
