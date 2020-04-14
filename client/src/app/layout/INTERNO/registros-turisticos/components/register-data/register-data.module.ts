462e856b132be7565b7a7c6839bb9e2d88039268
pasthortown <pastorchessmaster@gmail.com>
2020-04-13 20:02:13 -0500

    hola

--- client/src/app/layout/INTERNO/registros-turisticos/components/register-data/register-data.module.ts
+++ client/src/app/layout/INTERNO/registros-turisticos/components/register-data/register-data.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { AgmCoreModule } from '@agm/core';
+import { CKEditorModule } from 'ngx-ckeditor';
+import { NgxBarcodeModule } from 'ngx-barcode';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
+
 import { ClientDataModule } from './../../../../client-data-components/client-data.module';
 import { RegisterDataComponent } from './register-data.component';
 
 @NgModule({
   imports: [CommonModule, 
+    FormsModule,
+    NgbModule,
+    NgxBarcodeModule,
+    CKEditorModule,
+    AgmCoreModule.forRoot({apiKey: environment.gmapapiKey}),
+    NgxQRCodeModule,
+    Ng2TableModule,
+    ScrollToModule.forRoot(),
+    ClienteExternoRoutingModule,
     ClientDataModule
   ],
   declarations: [RegisterDataComponent],
