3ff51920bd80182b94fb6ae7d45ac071b532b3ef
pasthortown <pastorchessmaster@gmail.com>
2020-04-20 10:35:07 -0500

    hola

--- client/src/app/layout/INTERNO/registros-turisticos/components/coordinador/coordinador-asignacion-data/coordinador-asignacion-data.component.ts
+++ client/src/app/layout/INTERNO/registros-turisticos/components/coordinador/coordinador-asignacion-data/coordinador-asignacion-data.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Establishment } from 'src/app/models/BASE/Establishment';
+import { Ruc } from 'src/app/models/BASE/Ruc';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-coordinador-asignacion-data',
@@ -6,12 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coordinador-asignacion-data.component.scss']
 })
 export class CoordinadorAsignacionDataComponent implements OnInit {
-  
+  @Input('data_selected') data_selected = {row: null, 
+    register: {register: null,
+      activity_id: 0,
+      activity: '',
+      establishment: new Establishment(),
+      ruc: new Ruc(),
+      states: null,
+      register_data_on_catastro: null
+    }
+  };
+
+  @Input('tecnicos_financieros') tecnicosFinancieros = [];
+  @Input('tecnicos_zonales') tecnicosZonales = [];
+
   constructor() {
     
   }
 
   ngOnInit() {
-   
+    this.loadCatalogos();
+    this.refresh();
+  }
+  
+  ngOnChanges() {
+    this.refresh();
+  }
+
+  loadCatalogos() {
+
+  }
+
+  refresh() {
+    console.log(this.tecnicosFinancieros);
+    console.log(this.tecnicosZonales);
+    console.log(this.data_selected);
   }
 }
